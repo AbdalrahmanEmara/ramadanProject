cc02f6407616a7d085fe66f362bbca123e542a74
abdallah <abdallahnabil2003@gmail.com>
2025-03-12 02:49:02 +0200

    add books data

--- books and video slider scetions/books/js/main.js
+++ books and video slider scetions/books/js/main.js
@@ -1,14 +1,14 @@
 let books = [
-    { name: "book1", image: "./img/1.png" },
-    { name: "book2", image: "./img/2.png" },
-    { name: "book3", image: "./img/3.png" },
-    { name: "book4", image: "./img/4.png" },
-    { name: "book5", image: "./img/5.png" },
-    { name: "book6", image: "./img/6.png" },
-    { name: "book7", image: "./img/7.png" },
-    { name: "book8", image: "./img/8.png" },
-    { name: "book9", image: "./img/9.png" },
-    { name: "book10", image: "./img/10.png" }
+    { name: "مباحث في أصول الدين", image: "./img/1.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/do5UqLso9w11kvAs6fMsQUuBoQX06xebtX10ekxE.pdf"},
+    { name: "أسماء الله وصفاته", image: "./img/2.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/Q7Dx2ww1JWl8NY5cSNTqyhJVI7Yyfks7ePcuzutZ.pdf"},
+    { name: "الإبداع في كمال الشرع", image: "./img/3.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/OZoao0IWxhhe3YksNNBC7I7P8T0CRsw3bhI0geMt.pdf"},
+    { name: "التمسك بالسنة", image: "./img/4.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/feEwev0UUHiAmTu44G4RUUE1tlNiJ0FJmmUZmJCi.pdf"},
+    { name: "الخلاف بين العلماء", image: "./img/5.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/j88YUnmizlnSdsfKgLlp8XesJNlaqfBzvpeAdG4j.pdf"},
+    { name: "مكارم الأخلاق", image: "./img/6.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/2TSIEsiymWdSHNNTasU5ZLfKi9mYrtv0el44bdQh.pdf"},
+    { name: "من الأحكام الفقهية في الطهارة والصلاة والجنائز", image: "./img/7.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/h6GCK7mectZvd9oEyqSBcxUxFoTiazomb3MrfIRb.pdf"},
+    { name: "ستون سؤالا في أحكام الحيض والنفاس", image: "./img/8.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/udjv4bLWNVb3AbORgXOOAA0bdjWlknisUazsp7Dt.pdf"},
+    { name: "رسالة في مواقيت الصلاة", image: "./img/9.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/U4YtC46myREiRGBTt6IF8KeJhAt12Y3aKhCQ8SD4.pdf"},
+    { name: " رسالة في سجود السهو", image: "./img/10.png" ,url:"https://shekhcp.binothaimeen.net/storage/translated-books/2024-09-08/zgcKXDJ1lBKl5g69PdOrsVZ59Vfr6l055iocA5vk.pdf"}
 ];
 
 function displayBooks() {
@@ -22,7 +22,7 @@ function displayBooks() {
         bookItem.innerHTML = `
             <img src="${book.image}" alt="${book.name}">
             <p>${book.name}</p>
-            <button class="btn">المزيد</button>
+            <a class="btn" href="${book.url}">المزيد</a>
         `;
 
         gridContainer.appendChild(bookItem);
